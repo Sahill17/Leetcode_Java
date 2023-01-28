3c2e3ab010d1b2eac418cca043d17b5fe847ff1d
iamSahil <samael17111@gmail.com>
2023-01-29 00:04:31 +0530

    Update 342. Power of Four.java

--- Easy/342. Power of Four.java
+++ Easy/342. Power of Four.java
@@ -9,3 +9,12 @@ class Solution {
         return check(n, value * 4);
     }
 }
+
+
+// class Solution {
+//    public boolean isPowerOfFour(int n) {
+//        if(n<=0) return false;
+//        if((n&(n-1))!=0) return false;
+//         return (n-1)%3==0;
+//     }
+// }
