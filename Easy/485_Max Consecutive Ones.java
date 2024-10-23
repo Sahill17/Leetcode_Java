5e4a6935053a0bf722196d345c9bbad8dcc70c4f
Sahil17 <samael17111@gmail.com>
2024-10-23 19:30:39 +0530

    added for 485*

--- Easy/485_Max Consecutive Ones.java
+++ Easy/485_Max Consecutive Ones.java
@@ -1,3 +1,5 @@
+// While loop -> More efficient
+
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
          int maximum=0;
@@ -14,5 +16,24 @@ class Solution {
         i++;
         }
         return maximum; 
+    }
+}
+
+
+// For loop
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int current = 0;
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i]==1){
+                current++;
+            } else {
+                max = Math.max(max,current);
+                current=0;
+            }
+        }
+        return Math.max(max,current);
     }
 }
